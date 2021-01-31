47c5b67205a9425e5379c8b341f7f641eb25bf0a
Kirk Clarke <kclarke.design@gmail.com>
2021-01-31 01:33:47 -0500

    Adding logic for choosing image intent, started logic for retrieving selected images from user

--- app/src/main/java/com/kclarke/mymemory/CreateActivity.kt
+++ app/src/main/java/com/kclarke/mymemory/CreateActivity.kt
@@ -1,17 +1,31 @@
 package com.kclarke.mymemory
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kclarke.mymemory.models.BoardSize
 import com.kclarke.mymemory.utils.EXTRA_BOARD_SIZE
+import com.kclarke.mymemory.utils.isPermissionGranted
+import com.kclarke.mymemory.utils.requestPermission
 
 class CreateActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "CreateActivity"
+        private const val PICK_PHOTO_CODE = 973
+        private const val READ_EXTERNAL_PHOTOS_CODE = 336
+        private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+    }
 
     private lateinit var rvImagePicker: RecyclerView
     private lateinit var etGameName: EditText
@@ -35,9 +49,28 @@ class CreateActivity : AppCompatActivity() {
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Choose picture (0 / $numImagesRequired)"
 
-        rvImagePicker.adapter = ImagePickerAdapter(this, chosenImageUris, boardSize)
+        rvImagePicker.adapter = ImagePickerAdapter(this, chosenImageUris, boardSize, object: ImagePickerAdapter.ImageClickListener {
+            override fun onPlaceholderClicked() {
+                if ( isPermissionGranted(this@CreateActivity, READ_PHOTOS_PERMISSION)) {
+                    launchIntentForPhotos()
+                } else {
+                    requestPermission(this@CreateActivity, READ_PHOTOS_PERMISSION, READ_EXTERNAL_PHOTOS_CODE)
+                }
+            }
+        })
         rvImagePicker.setHasFixedSize(true)
         rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == READ_EXTERNAL_PHOTOS_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                launchIntentForPhotos()
+            } else {
+                Toast.makeText(this, "To create a custom game, you must provide access to your photos", Toast.LENGTH_LONG).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -46,5 +79,28 @@ class CreateActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode != PICK_PHOTO_CODE || resultCode != Activity.RESULT_OK || data == null) {
+            Log.w(TAG, "No data received after launching activity")
+            return
+        }
+        val selectedUri = data.data
+        val clipData = data.clipData
+        if (clipData != null) {
+            Log.w(TAG, "clipDate numImages ${clipData.itemCount}: $clipData")
+            for (i in 0 until clipData.itemCount) {
+                val clipItem = clipData.getItemAt(i)
+            }
+        }
+    }
+
+    private fun launchIntentForPhotos() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        startActivityForResult(Intent.createChooser(intent, "Choose your pics"), PICK_PHOTO_CODE)
     }
 }
